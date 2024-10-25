de9ced614ee16344588036fa959a391109dc6794
LefMono000 <a2-supremacy@outlook.com>
2024-10-24 19:02:33 -0600

    Deploy React app to gh-pages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          Jimmy eres un reverendo pendejo. Viva Cioccolata y las manboobs.
+          Jimmy eres un reverendo pendejo. Viva Cioccolata y sus tetotas.
         </h1>
         <a
           className="App-link"
